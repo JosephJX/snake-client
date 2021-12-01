2a0b4c0d540492b800e759f16a62d7ba95a3918f
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-01 17:46:45 -0500

    exported connection function to play

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "Localhost",// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.on("connect", () => {
+      console.log("Connection established")
+  });
+
+  conn.on("data", (data) => {
+    console.log(data)
+  });
+
+
+  conn.on("close", () => {
+      console.log("Connection closed")
+  });
+
+  // //conn.on("event name", () => {
+
+  // };
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = {connect}
--- play.js
+++ play.js
@@ -1,34 +1,5 @@
-const net = require("net");
-
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "Localhost",// IP address here,
-    port: 50541// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.on("connect", () => {
-      console.log("Connection established")
-  });
-
-  conn.on("data", (data) => {
-    console.log(data)
-  });
-
-
-  conn.on("close", () => {
-      console.log("Connection closed")
-  });
-
-  // //conn.on("event name", () => {
-
-  // };
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require("./client")
 
 console.log("Connecting ...");
 connect();
