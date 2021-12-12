6433c78a84639205a980344462fc1e78ac96da2e
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-12 15:13:25 -0500

    setupInput and handleUserInput

--- play.js
+++ play.js
@@ -2,4 +2,22 @@
 const { connect } = require("./client")
 
 console.log("Connecting ...");
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (key) {
+  const stdout = process.stdout;
+  if (key === '\u0003') {
+    stdout.write("Exited snek game. Bye bye.\n");
+    process.exit()
+  }
+};
+
+setupInput();
+
 connect();
